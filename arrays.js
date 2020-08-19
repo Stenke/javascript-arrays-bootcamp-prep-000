8536756cf579fc48902d65d427a7f5f72280eca1
Stephen Enke <stephenenke@gmail.com>
2020-08-19 19:10:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
- array = [1];
- element = 'foo';
  arrayNew = [element, ...array]
  return arrayNew
 }
