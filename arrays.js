75df79c9d2136d1e4e31d648f174b688f4331793
Stephen Enke <stephenenke@gmail.com>
2020-08-19 19:18:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array
 }
-function accessElementInArray(array, element) {
+function accessElementInArray(array, index) {
   
 }
