32ef6df65fe05d817c3602ae621d66fc4334e89e
Stephen Enke <stephenenke@gmail.com>
2020-08-19 18:52:38 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array, element) {
+ var array = [1];
+ var element = 'foo';
+ arrayNew = [element, ...array]
+ return arrayNew
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var array = [1];
+  element = 'foo'
+  array.unshift(element);
+  return array
+}
